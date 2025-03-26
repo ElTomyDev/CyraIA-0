e7e6cb2269d60d5fac4ca158d3dae09ace6b8643
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-03-26 12:32:15 -0300

    Update general_config.py

--- config/general_config.py
+++ config/general_config.py
@@ -4,4 +4,4 @@ WINDOWS_HEIGHT = 700 # Alto de la ventana
 
 FPS = 60 # Frames por segundo
 
-BEST_MODEL_PATH = "./trained_models/best_model.pth" # Ruta al mejor modelo actual
+BEST_MODEL_PATH = "./cyraai_models/best_model.pth" # Ruta al mejor modelo actual
